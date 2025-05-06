d70fa1f5a0a4468886509df506a56d9019761c58
Alfonsas <alfonsasalubeckas@gmail.com>
2025-05-06 16:52:18 +0300

    Register, login, profile pages implemented.

--- src/pages/auth/LoginPage.tsx
+++ src/pages/auth/LoginPage.tsx
@@ -1,7 +1,51 @@
+import { useState } from "react"
+import { API_URL } from "../../utils/config"
+import { useNavigate } from "react-router"
+import { useAuth } from "../../AuthContext"
+import api from "../../api"
+
 const LoginPage: React.FC = () => {
+    const { loginUser } = useAuth()
+    const [ email, setEmail ] = useState('')
+    const [ password, setPassword ] = useState('')
+
+    const navigate = useNavigate()
+
+    const userEmailHandler = (event: React.ChangeEvent<HTMLInputElement>) => setEmail(event.target.value)
+    const userPasswordHandler = (event: React.ChangeEvent<HTMLInputElement>) => setPassword(event.target.value)
+
+    const loginHandler = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+        
+        try {
+            const loginInfo = { email, password }
+            const res = await api.post(`${API_URL}/users/login`, loginInfo)
+            const { token } = res.data
+
+            if (token) {
+                loginUser(token)
+                navigate('/dashboard/profile')
+            }
+    
+        } catch (error) {
+            console.log('Failed to register', error)
+        }
+    }
+
     return (
         <div>
-            <h1>Login Page</h1>
+            <h1>Login</h1>
+            <form onSubmit={loginHandler}>
+                <div className="formControl">
+                    <label htmlFor="email">Email:</label>
+                    <input type="email" name="email" id="email" value={email} onChange={userEmailHandler} />
+                </div>
+                <div className="formControl">
+                    <label htmlFor="password">Password:</label>
+                    <input type="password" name="password" id="password" value={password} onChange={userPasswordHandler} />
+                </div>
+                <button type="submit">Login</button>
+            </form>
         </div>
     )
 }
--- src/pages/auth/RegisterPage.tsx
+++ src/pages/auth/RegisterPage.tsx
@@ -1,7 +1,49 @@
+import { useState } from "react"
+import { API_URL } from "../../utils/config"
+import { useNavigate } from "react-router"
+import api from "../../api"
+
 const RegisterPage: React.FC = () => {
+    const [ username, setUsername ] = useState('')
+    const [ email, setEmail ] = useState('')
+    const [ password, setPassword ] = useState('')
+
+    const navigate = useNavigate()
+
+    const userNameHandler = (event: React.ChangeEvent<HTMLInputElement>) => setUsername(event.target.value)
+    const userEmailHandler = (event: React.ChangeEvent<HTMLInputElement>) => setEmail(event.target.value)
+    const userPasswordHandler = (event: React.ChangeEvent<HTMLInputElement>) => setPassword(event.target.value)
+
+    const registerHandler = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+        
+        try {
+            const userInfo = { username, email, password }
+            await api.post(`${API_URL}/users/register`, userInfo)
+            navigate('/login')
+        } catch (error) {
+            console.log('Failed to register', error)
+        }
+    }
+
     return (
         <div>
-            <h1>Register Page</h1>
+            <h1>Register</h1>
+            <form onSubmit={registerHandler}>
+                <div className="formControl">
+                    <label htmlFor="username">User name:</label>
+                    <input type="text" name="username" id="username" value={username} onChange={userNameHandler} />
+                </div>
+                <div className="formControl">
+                    <label htmlFor="email">Email:</label>
+                    <input type="email" name="email" id="email" value={email} onChange={userEmailHandler} />
+                </div>
+                <div className="formControl">
+                    <label htmlFor="password">Password:</label>
+                    <input type="password" name="password" id="password" value={password} onChange={userPasswordHandler} />
+                </div>
+                <button type="submit">Register</button>
+            </form>
         </div>
     )
 }
