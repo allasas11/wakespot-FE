e602822a01ef7f0945c1877bd63b4db1b60115db
Alfonsas <alfonsasalubeckas@gmail.com>
2026-01-15 20:42:23 +0200

    api url for vercel updated

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,1 +1,3 @@
-export const API_URL = 'http://localhost:3015/api'
+// export const API_URL = 'http://localhost:3015/api || http://localhost:10000'
+
+export const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3015/api';
